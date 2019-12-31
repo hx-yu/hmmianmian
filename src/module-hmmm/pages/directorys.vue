8caa110fdf66d3901a895ddc2d1af9f6da8ea8e7
renqingguo <renqingguo@rqg>
2019-12-31 17:30:14 +0800

    样式完成

--- src/module-hmmm/pages/directorys.vue
+++ src/module-hmmm/pages/directorys.vue
@@ -3,28 +3,16 @@
     <div class="app-container">
       <!-- 上 -->
       <el-card>
-        <el-form :inline="true">
-          <!-- 目录 -->
-          <el-form-item label="目录名称">
-            <el-select style="margin-right:10px;" v-model="value" placeholder="请选择">
-              <el-option
-                v-for="item in 4"
-                :key="item"
-                :label="item.label"
-                :value="item.value"
-              ></el-option>
-            </el-select>
-          </el-form-item>
-          <!-- 状态 -->
-          <el-form-item label="状态">
-            <el-select></el-select>
-          </el-form-item>
-          <!-- 按钮 -->
-          <el-form-item>
-            <el-button style="width:90px;margin:0 10px;">清除</el-button>
-          </el-form-item>
-          <el-form-item>
-            <el-button style="width:90px" type="primary">搜索</el-button>
+        <!-- 按钮 -->
+        <el-row slot="header">
+          <el-button class="buttons" type="primary">新增目录</el-button>
+          <el-button type="primary">返回学科</el-button>
+        </el-row>
+        <el-form style="margin-top:20px">
+          <el-form-item label="目录名称" label-width="100px">
+            <el-input style="width:230px;margin-right:10px"></el-input>
+            <el-button>清除</el-button>
+            <el-button type="primary">搜索</el-button>
           </el-form-item>
         </el-form>
       </el-card>
@@ -32,14 +20,13 @@
       <el-card class="underCard">
         <!-- <el-row slot="header"></el-row> -->
         <el-table :data="listCatalog" style="width: 100%">
-          <el-table-column  prop="id" label="序号" ></el-table-column>
-          <el-table-column  prop="directoryName" label="目录名称" ></el-table-column>
-          <el-table-column  prop="creator" label="创建者" ></el-table-column>
-          <el-table-column  prop="addDate" label="创建日期" ></el-table-column>
-          <el-table-column  prop="totals" label="面试题数量" ></el-table-column>
-          <el-table-column :formatter='statesTab' prop="state"  label="状态" >
-          </el-table-column>
-          <el-table-column   label="操作" >
+          <el-table-column prop="id" label="序号"></el-table-column>
+          <el-table-column prop="directoryName" label="目录名称"></el-table-column>
+          <el-table-column prop="username" label="创建者"></el-table-column>
+          <el-table-column prop="addDate" label="创建日期"></el-table-column>
+          <el-table-column prop="totals" label="面试题数量"></el-table-column>
+          <el-table-column :formatter="statesTab" prop="state" label="状态"></el-table-column>
+          <el-table-column label="操作">
             <span>
               <el-button type="text" size="small">修改</el-button>
               <el-button type="text" size="small">禁用</el-button>
@@ -68,20 +55,21 @@ import { log } from "util";
 export default {
   data() {
     return {
-      listCatalog:[] ,//目录列表,
-    }
+      listCatalog: [] //目录列表,
+    };
   },
   methods: {
-    statesTab(row,column,cellValue,index){
-      console.log(row);
-      return row.state===1? '开启': '屏蔽'
-      
+    // 状态设置方法
+    statesTab(row, column, cellValue, index) {
+      // console.log(row);
+      return row.state === 1 ? "开启" : "屏蔽";
     }
   },
   created() {
+    // 获取表格列表
     list().then(res => {
       console.log(res);
-      this.listCatalog = res.data.items.reverse()
+      this.listCatalog = res.data.items.reverse();
     });
   }
 };
@@ -91,7 +79,10 @@ export default {
 // .pagination{
 //   // height: 80px;
 // }
-.underCard{
+.underCard {
   margin-top: 20px;
+}
+.buttons {
+  text-align: center;
 }
 </style>
