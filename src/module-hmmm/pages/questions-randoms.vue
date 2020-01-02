80d625ea9af64be97bf9d9e2c829018be0feee70
wangziheng <wangprint@yeah.net>
2020-01-02 11:36:33 +0800

    组题列表完成

--- src/module-hmmm/pages/questions-randoms.vue
+++ src/module-hmmm/pages/questions-randoms.vue
@@ -2,30 +2,46 @@
   <!-- <div class="dashboard-container">
     <div class="app-container">组题列表111</div>
   </div> -->
-  <el-table border :data="tableData">
-    <el-table-column align='center' width="50px" type="index" label="序号"></el-table-column>
-    <el-table-column align='center' prop="addTime" label="组题时间"></el-table-column>
-    <el-table-column align='center' width="100px" prop="userName" label="用户名"></el-table-column>
-    <el-table-column align='center' width="180px" prop="id" label="试题ID"></el-table-column>
-    <el-table-column align='center' prop="progressOfAnswer" label="答题进度"></el-table-column>
-    <el-table-column align='center' prop="accuracyRate" label="正确率"></el-table-column>
-    <el-table-column align='center' prop="totalSeconds" label="答题总耗时（秒）"></el-table-column>
-    <el-table-column align='center' prop="questionType" label="组题类型/详情"></el-table-column>
-    <el-table-column align='center' label="操作">
-      <el-button type="text">删除</el-button>
-    </el-table-column>
-  </el-table>
+  <el-card>
+      <el-table border :data="tableData">
+        <el-table-column align='center' width="50px" type="index" label="序号"></el-table-column>
+        <el-table-column align='center' prop="addTime" label="组题时间"></el-table-column>
+        <el-table-column align='center' width="100px" prop="userName" label="用户名"></el-table-column>
+        <el-table-column align='center' width="180px" prop="id" label="试题ID"></el-table-column>
+        <el-table-column align='center' prop="progressOfAnswer" label="答题进度"></el-table-column>
+        <el-table-column align='center' prop="accuracyRate" label="正确率"></el-table-column>
+        <el-table-column align='center' prop="totalSeconds" label="答题总耗时（秒）"></el-table-column>
+        <el-table-column align='center' prop="questionType" label="组题类型/详情"></el-table-column>
+        <el-table-column align='center' label="操作">
+          <el-button type="text" @click="removeRandom">删除</el-button>
+        </el-table-column>
+      </el-table >
+      <el-row  
+          style="height:100px;"
+          type="flex"
+          justify="center"
+          align="middle">
+          <el-pagination
+              background
+              layout="prev, pager, next"
+              :total="pageList.counts"  
+              :page-count="pageList.pages"
+              :page-size="pageList.pageSize"
+              :current-page="pageList.page">
+          </el-pagination>
+      </el-row>
+  </el-card>
 </template>
 
 <script>
-import {randoms} from '../../api/hmmm/questions'
+import {randoms,removeRandoms} from '../../api/hmmm/questions'
 export default {
   name: 'QuestionsRandoms',
   data() {
     return {
       tableData:[],//接收列表信息
-      pageLit:{  
-        counst:0,//总记录数
+      pageList:{  
+        counts:0,//总记录数
         pageSize:10,//每页数量
         pages:0,//总页数
         page:1,//当前页码 
@@ -35,16 +51,28 @@ export default {
   methods:{
   async getPageMessage(){
     let result = await randoms()
-    console.log(result);
-    
+    // console.log(result);
     result.data.items.forEach((item,index) => {
       // console.log(item);
       item.progressOfAnswer=item.progressOfAnswer+"%"
       item.accuracyRate=item.accuracyRate+"%"
-      console.log(item);
+      // console.log(item);
     });
     this.tableData=result.data.items
-    this.tableData
+    this.pageList.counts=result.data.counts
+    this.pageList.pagesize=result.data.pagesize
+    this.pageList.pages=result.data.pages
+    // this.pageList.page=result.data.pages
+    },
+    async removeRandom(){
+      await this.$confirm("您确认要删除吗")
+      await removeRandoms(this.tableData)
+         this.$message({
+           type:"success",
+           message:"删除成功"
+         })
+
+
     }
   },
   created(){ 
@@ -55,4 +83,5 @@ export default {
 </script>
 
 <style scoped lang='less'>
+
 </style>
